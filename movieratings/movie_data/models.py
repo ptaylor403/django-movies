0afca10e7a533caa873d78e5427e69f828a134b1
Trisha <ptaylor403@gmail.com>
2016-05-04 15:42:02 -0400

    added models for movie lens data

--- movieratings/movie_data/models.py
+++ movieratings/movie_data/models.py
@@ -1,14 +1,21 @@
 from django.db import models
 
 class Movie(models.Model):
-    pass
-
-
-
-class Rating(models.Model):
-    pass
-
-
+    movie_id = model.IntegerField(default=0)
+    movie = model.CharField(max_length=200)
+    release_date = model.DatetimeField()
+    video_release_date = model.DatetimeField()
+    IMDb_url = model.CharField(max_length=200)
 
 class Rater(models.Model):
-    pass
+    rater_id = model.IntegerField(default=0)
+    age = model.IntegerField(default=0)
+    gender = model.CharField(max_length=None)
+    occupation = model.CharField(max_length=30)
+    zipcode = model.IntegerField(default=0)
+
+class Rating(models.Model):
+    rater_id = model.ForeignKey(Rater)
+    movie_id = model.ForeignKey(Movie)
+    rating = model.IntegerField(default=0)
+    timestamp = model.IntegerField(default=0)
